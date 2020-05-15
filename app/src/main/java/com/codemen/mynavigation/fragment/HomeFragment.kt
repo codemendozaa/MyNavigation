255342ac4fe63363d8b15483085a78f06678d7e5
erixjavier <mendozaeria@globalhitss.com>
2020-05-15 13:00:44 -0500

    navegation:creacion de los fragment e implementacion host fragment

---
+++ app/src/main/java/com/codemen/mynavigation/fragment/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.codemen.mynavigation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.codemen.mynavigation.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonfirstFragment.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.firstFragment2)
+        }
+
+        buttonSecondFragment.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.secondFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/codemen/mynavigation/fragment/SecondFragment.kt
@@ -0,0 +1,23 @@
+package com.codemen.mynavigation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codemen.mynavigation.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+}
