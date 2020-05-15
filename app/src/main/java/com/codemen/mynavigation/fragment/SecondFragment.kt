5c59b7035a48d7224b0cde333e0a2f1fa69b2cf7
erixjavier <mendozaeria@globalhitss.com>
2020-05-15 14:49:50 -0500

    navegation: implementacion de up Button to the AppBar a en cada fragment

--- app/src/main/java/com/codemen/mynavigation/fragment/SecondFragment.kt
+++ app/src/main/java/com/codemen/mynavigation/fragment/SecondFragment.kt
@@ -7,16 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.codemen.mynavigation.R
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
