31a61eabd58abf6c444816b9d0972c9c39175dd7
erixjavier <mendozaeria@globalhitss.com>
2020-05-15 14:31:17 -0500

    navegation: implementacion de up Button to the AppBar a en cada fragment

--- app/src/main/java/com/codemen/mynavigation/MainActivity.kt
+++ app/src/main/java/com/codemen/mynavigation/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.codemen.mynavigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = this.findNavController(R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+
     }
 }
+
+
